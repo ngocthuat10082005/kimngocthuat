b2973cdc916e5bf89b68fe1146973173b6a06e1c
ngocthuat10082005 <147481132+ngocthuat10082005@users.noreply.github.com>
2023-10-17 16:26:38 +0700

    Add files via upload

--- baithuchanh2mang.cpp
+++ baithuchanh2mang.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<conio.h>
-
+/*
 int main(){
-	int a[100],n,i,max=i,x;
+	int a[100],n,i;
 	
 
 	
@@ -14,15 +14,40 @@ int main(){
 		scanf("%d",&a[i]);
 		}
 		getch();
-
+ 
 for(i=0;i<n;i++){
 	printf("\n phan tu thu a[%d]=%d",i,a[i]);
 	}
-	for(x=0;x<i;i++)
-	{if(a[i]>=max)
-	max=a[i];}
-	printf("\n phan tu lon nhat la=%d",max);
-	getch();
 		
 }
- 
+
+int main(){
+
+ int max=0, i;
+ int a[5]={1,2,3,4,5};
+ for( i=0; i<5; i++)
+ {
+ 	if(a[i]>=max)
+ 	max=a[0];
+ }
+ 	printf("\n phan tu lon nhat la = %d", max);
+ 	getch();
+}
+*/
+int main()
+{
+	int tong =0,tich=1,dem=0,i;
+	int a[5]={1,2,3,4,5};
+	for( i=0; i<5; i++)
+	{
+	tong=tong+a[i];
+		tich=tich*a[i];
+		dem=dem+1
+}
+	
+	printf("\n tong mang =%d",tong);
+	printf("\n tich mang =%d",tich);
+	printf("\n so phan tu la=%d",dem);
+	grt
+}
+
